86734159c6508a45ffdc0854d63eb38ea5363d1d
priti <priti.sharma@sjsu.edu>
2020-10-22 14:06:51 -0700

    react app commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "dropbox-281"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://owk06lx51a.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_XiXobq49o",
+    APP_CLIENT_ID: "6snhl8f3n62uro8h1s9p94hqju",
+    IDENTITY_POOL_ID: "us-east-2:7f08bc48-d417-4a67-b55d-34529946d063"
+  }
+};
